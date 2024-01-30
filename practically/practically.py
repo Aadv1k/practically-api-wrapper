ef98e76d902bd488c395156a600deec72ad3cc59
Aadvik <aadv1k@outlook.com>
2024-01-30 14:34:40 +0530

    Add functionality to reuse sessions

--- practically/practically.py
+++ practically/practically.py
@@ -2,33 +2,67 @@ import requests
 import os
 from .utils import str_to_md5
 from .api.user import User
+import pickle
 
 class Practically:
-    def __init__(self, base_url="https://teach.practically.com"):
+    def __init__(self, base_url="https://teach.practically.com", session_file="session.pickle"):
         self.base_url = base_url
         self.session_id = None
         self.user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:122.0) Gecko/20100101 Firefox/122.0"
+        self.session_file = session_file
+
+    def __dump_session(self):
+        with open(self.session_file, "wb") as f:
+            pickle.dump({
+                "session_id": self.session_id
+            }, f)
+
+    def __load_session(self):
+        try:
+            with open(self.session_file, "rb") as f:
+                data = pickle.load(f)
+                session_id = data.get("session_id")
+
+                if not session_id:
+                    raise Exception
+
+                return session_id
+        except:
+            return None
+
+    def is_session_expired_or_invalid(self):
+        try:
+            res = requests.get(f"{self.base_url}/v1/studentweb/myschool/dashboard", headers={
+                "Cookie": f"ci_session={self.session_id}"
+            })
+            res.raise_for_status()
+            return not res.ok
+        except requests.RequestException:
+            return True
 
     def create_session(self, username, password):
-        res = requests.post(
-            "https://teach.practically.com/v1/teacherapp_v1/loginWithPassword",
-            headers={
-                "User-Agent": self.user_agent,
-                "Content-Type": "application/x-www-form-urlencoded"
-            },
-            data={
-                "LoginID": username,
-                "Password": str_to_md5(password),
-                "IsWebRequest": "Y"
-            }
-        )
+        found = self.__load_session()
 
-        session_id = res.cookies.get("ci_session")
-        self.session_id = session_id
-        return None
+        if not found or self.is_session_expired_or_invalid():
+            res = requests.post(
+                f"{self.base_url}/v1/teacherapp_v1/loginWithPassword",
+                headers={
+                    "User-Agent": self.user_agent,
+                    "Content-Type": "application/x-www-form-urlencoded"
+                },
+                data={
+                    "LoginID": username,
+                    "Password": str_to_md5(password),
+                    "IsWebRequest": "Y"
+                }
+            )
 
-    def is_session_expired(self):  # Added self parameter
-        pass
+            session_id = res.cookies.get("ci_session")
+
+            self.session_id = session_id
+            self.__dump_session()
+        else:
+            self.session_id = found
 
     def __get_secure(self, url):
         res = requests.get(f"{self.base_url}{url}", headers={
--- tests/test_user.py
+++ tests/test_user.py
@@ -1,4 +1,6 @@
 from dotenv import load_dotenv
+import pathlib
+
 load_dotenv()
 
 from practically.practically import Practically
@@ -6,9 +8,14 @@ from practically.practically import Practically
 p = Practically()
 
 def test_if_user_can_login():
-    #p.create_session_from_env("USERNAME", "PASSWORD")
-
-    p.session_id = "b14ef77393091fd4b8d9d0e4b53247ffa4dd7988"
+    p.create_session_from_env("USERNAME", "PASSWORD")
 
     user = p.get_user()
-    assert str(user) is not None
+    assert len(user.first_name) > 1
+
+def test_should_work_if_session_is_invalid():
+    p.session_id = "foobarbaz"
+    p.create_session_from_env("USERNAME", "PASSWORD")
+
+    user = p.get_user()
+    assert len(user.last_name) > 1
