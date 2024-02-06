211cde3fae133639b2def752c989bf4240417041
aadv1k <aadv1k@outlook.com>
2024-02-06 18:16:46 +0530

    Temporarily remove github workflows

--- tests/test_user.py
+++ tests/test_user.py
@@ -7,17 +7,17 @@ from practically.practically import Practically
 p = Practically()
 p.create_session_from_env("USERNAME", "PASSWORD")
 
+def test_if_session_is_valid():
+    assert p.is_session_expired_or_invalid() is False
 
-def test_if_user_can_login():
+def test_if_user_info_can_be_fetched():
     user = p.get_user()
-    assert len(user.first_name) > 1
+    assert user.first_name is not None
 
-
-def test_should_work_if_session_is_invalid():
+def test_if_session_is_invalid():
     p.session_id = "foobarbaz"
     p.create_session_from_env("USERNAME", "PASSWORD")
 
     user = p.get_user()
 
-    assert user is not None
-    assert len(user.last_name) > 1
+    assert user.first_name is not None
