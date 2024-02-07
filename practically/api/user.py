8ba8073b872911edfa7ef0d696c2cdfd393b32d7
Aadvik <aadv1k@outlook.com>
2024-02-07 20:02:27 +0530

    Implement more fields for `User` class

--- practically/api/user.py
+++ practically/api/user.py
@@ -1,23 +1,44 @@
 from bs4 import BeautifulSoup
 
+
 class User:
     def __init__(self, html: str):
         self.soup = BeautifulSoup(html, 'html.parser')
 
-    @property
-    def email(self) -> str | None:
-        html_input_elem = self.soup.find("input", {"id": "Email"})
+    def _find_input_value(self, input_id: str) -> str | None:
+        html_input_elem = self.soup.find("input", {"id": input_id})
         return html_input_elem['value'] if html_input_elem else None
 
     @property
+    def email(self) -> str | None:
+        return self._find_input_value("Email")
+
+    @property
     def first_name(self) -> str | None:
-        html_input_elem = self.soup.find("input", {"id": "FirstName"})
-        return html_input_elem['value'] if html_input_elem else None
-        
+        return self._find_input_value("FirstName")
+
     @property
     def last_name(self) -> str | None:
-        html_input_elem = self.soup.find("input", {"id": "LastName"})
-        return html_input_elem['value'] if html_input_elem else None
+        return self._find_input_value("LastName")
+
+    @property
+    def phone_number(self) -> str | None:
+        html_input_elem = self.soup.find("input", {"id": "Mobile"})
+        raw_phone_number = html_input_elem['value'] if html_input_elem else None
+
+        html_select_elem = self.soup.find("select", {"id": "country_code"})
+        raw_country_code = html_select_elem.find(
+            "option", {"selected": True}).text.strip() if html_select_elem else None
+
+        return f"{raw_country_code} {raw_phone_number}" if raw_country_code else raw_phone_number
+
+    @property
+    def display_name(self) -> str | None:
+        return self._find_input_value("DisplayName")
+
+    @property
+    def login_id(self) -> str | None:
+        return self._find_input_value("LoginID")
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
--- tests/test_user.py
+++ tests/test_user.py
@@ -1,4 +1,6 @@
 from dotenv import load_dotenv
+import re
+import pytest
 
 load_dotenv()
 
@@ -7,13 +9,32 @@ from practically.practically import Practically
 p = Practically()
 p.create_session_from_env("USERNAME", "PASSWORD")
 
+@pytest.mark.skip
 def test_if_session_is_valid():
     assert p.is_session_expired_or_invalid() is False
 
+user = p.get_user()
+
+@pytest.mark.skip
 def test_if_user_info_can_be_fetched():
-    user = p.get_user()
     assert user.first_name is not None
 
+def test_if_user_has_all_attributes():
+    assert user.first_name is not None
+    assert user.last_name is not None
+    assert user.display_name is not None
+
+    assert user.phone_number is not None
+    assert user.login_id is not None
+    assert user.email is not None
+
+    email_regex = r'^[\w\.-]+@[\w\.-]+\.\w+$'
+    assert re.match(email_regex, user.email), f"Email '{user.email}' does not match the expected format."
+
+    phone_number_regex = r'^\+\d{1,3}\s\d{3,}$'
+    assert re.match(phone_number_regex, user.phone_number), f"Phone number '{user.phone_number}' does not match the expected format."
+
+@pytest.mark.skip
 def test_if_session_is_invalid():
     p.session_id = "foobarbaz"
     p.create_session_from_env("USERNAME", "PASSWORD")
