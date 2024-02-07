a5509aa86796509c2cf9eaaf049036960a745830
Aadvik <aadv1k@outlook.com>
2024-02-07 20:08:05 +0530

    Mark the tests as not being being skipped
    we [skip ci] since we haven't implemented caching yet

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,6 +1,5 @@
 from dotenv import load_dotenv
 import re
-import pytest
 
 load_dotenv()
 
@@ -9,15 +8,17 @@ from practically.practically import Practically
 p = Practically()
 p.create_session_from_env("USERNAME", "PASSWORD")
 
-@pytest.mark.skip
+
 def test_if_session_is_valid():
     assert p.is_session_expired_or_invalid() is False
 
+
 user = p.get_user()
 
-@pytest.mark.skip
+
 def test_if_user_info_can_be_fetched():
     assert user.first_name is not None
+
 
 def test_if_user_has_all_attributes():
     assert user.first_name is not None
@@ -28,13 +29,17 @@ def test_if_user_has_all_attributes():
     assert user.login_id is not None
     assert user.email is not None
 
-    email_regex = r'^[\w\.-]+@[\w\.-]+\.\w+$'
-    assert re.match(email_regex, user.email), f"Email '{user.email}' does not match the expected format."
+    email_regex = r"^[\w\.-]+@[\w\.-]+\.\w+$"
+    assert re.match(
+        email_regex, user.email
+    ), f"Email '{user.email}' does not match the expected format."
 
-    phone_number_regex = r'^\+\d{1,3}\s\d{3,}$'
-    assert re.match(phone_number_regex, user.phone_number), f"Phone number '{user.phone_number}' does not match the expected format."
+    phone_number_regex = r"^\+\d{1,3}\s\d{3,}$"
+    assert re.match(
+        phone_number_regex, user.phone_number
+    ), f"Phone number '{user.phone_number}' does not match the expected format."
 
-@pytest.mark.skip
+
 def test_if_session_is_invalid():
     p.session_id = "foobarbaz"
     p.create_session_from_env("USERNAME", "PASSWORD")
